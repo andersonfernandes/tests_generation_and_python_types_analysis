fb912b6b723f2d8632f1c72d08a21a7a69f3a92c
Anderson Fernandes <fernandesanderson14@gmail.com>
2023-05-25 20:56:46 -0300

    Add gh scrapper initial script
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ github_scrapper.py
@@ -0,0 +1,8 @@
+import os
+from github import Github
+
+github = Github(os.getenv('GITHUB_TOKEN'))
+
+repositories = github.search_repositories(query='language:python archived:NOT is:public stars:>10000 forks:>500')
+for repo in repositories.get_page(1):
+    print(repo.name, repo.html_url)
